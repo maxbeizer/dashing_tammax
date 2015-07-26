6388346c00b7153179df145599bbc819159049f2
Tamara Scott <max.beizer@gmail.com>
2015-07-26 10:46:04 -0500

    Update next bus to use stop instead of line

--- jobs/next_bus.rb
+++ jobs/next_bus.rb
@@ -1,7 +1,6 @@
-SCHEDULER.every '30m', first_in: 0 do |job|
+SCHEDULER.every '20m', first_in: 0 do |job|
   send_event 'next_bus', {
-    route_three: NextBus.three,
-    route_five: NextBus.five,
-    route_ten: NextBus.ten
+    west_end: NextBus.west_end,
+    charlotte: NextBus.charlotte
   }
 end
--- lib/next_bus.rb
+++ lib/next_bus.rb
@@ -1,41 +1,30 @@
-require 'pry'
 require 'net/http'
 require 'uri'
 require 'json'
 require 'time'
 
 module NextBus
-  BASE_URI                       = 'http://nextbus.jt2k.com/api/route/'
-  DIRECTION                      = '/dir/1/stop/'
+  BASE_URI                       = 'http://nextbus.jt2k.com/api/stop/'
   PARAMS                         = "?key=#{ENV['NEXT_BUS_API_KEY']}"
   CHARLOTTE_AND_46TH_STOP_ID     = 'CXIRICHL'
   WEST_END_AND_ELMINGTON_STOP_ID = 'WESELMEN'
-  STOP_MAP = {
-    '3'  => WEST_END_AND_ELMINGTON_STOP_ID,
-    '5'  => WEST_END_AND_ELMINGTON_STOP_ID,
-    '10' => CHARLOTTE_AND_46TH_STOP_ID
-  }
 
   class << self
     attr_accessor :uri
 
-    def three
-      next_stop 3
+    def west_end
+      next_stop WEST_END_AND_ELMINGTON_STOP_ID
     end
 
-    def five
-      next_stop 5
-    end
-
-    def ten
-      next_stop 10
+    def charlotte
+      next_stop CHARLOTTE_AND_46TH_STOP_ID
     end
 
     private
-    def next_stop(line_no)
-      set_uri line_no.to_s
+    def next_stop(stop_id)
+      set_uri stop_id
       stop = upcoming_stops.first || first_stop_of_day
-      stop['arrival_time_str']
+      "#{stop['arrival_time_str']} (#{stop['route_id']})"
     end
 
     def upcoming_stops
@@ -58,8 +47,8 @@ module NextBus
       response(uri).body
     end
 
-    def set_uri(line_no='')
-      @uri = URI.parse(BASE_URI + line_no + DIRECTION + STOP_MAP[line_no] + PARAMS)
+    def set_uri(stop_id='')
+      @uri = URI.parse(BASE_URI + stop_id + PARAMS)
     end
 
     def response(uri)
